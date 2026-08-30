34dd267e7ba8cf891d496afa0621a93d5f06149f
Eric Chevallet <eric.chevallet@tsunaimi.ai>
2026-08-30 12:43:24 -0400

    Added version number

--- apps/frontend/website/static/30-envsubst-index.sh
+++ apps/frontend/website/static/30-envsubst-index.sh
@@ -2,7 +2,13 @@
 set -eu
 
 : "${NEXT_PUBLIC_ASK_MY_ENVOY_URL:?NEXT_PUBLIC_ASK_MY_ENVOY_URL is required}"
+RAW_VERSION="${VERSION:-dev}"
+case "${RAW_VERSION}" in
+  v*) SITE_VERSION="${RAW_VERSION}" ;;
+  *) SITE_VERSION="v${RAW_VERSION}" ;;
+esac
+export SITE_VERSION
 
-envsubst '${NEXT_PUBLIC_ASK_MY_ENVOY_URL}' \
+envsubst '${NEXT_PUBLIC_ASK_MY_ENVOY_URL} ${SITE_VERSION}' \
   < /usr/share/nginx/html/index.html.template \
   > /usr/share/nginx/html/index.html
